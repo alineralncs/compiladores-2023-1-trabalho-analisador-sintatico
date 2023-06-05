3687ac3b2d524014a2681fa323d2c75d5ba5adaa
alineralncs <alineralncs@gmail.com>
2023-06-05 13:52:01 -0300

    :test_tube: Teste parser

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from lexico.scanner import Scanner
 from lexico.scanner import *
+from sintatico.parser import Parser
 
+# if __name__ == '__main__':
+#     file_code_font = 'exemplos.md'
 
+#     scanner = Scanner(file_code_font)
+#     tokens, errors = scanner.analyse()
+
+#     print("\nTokens Válidos \n")
+#     for token in tokens:
+#         print('<{}, {}>'.format(token[0], token[1]))
+
+#     # print("\nTokens Inválidos: \n")
+#     # for error in errors:
+#     #     print(errors[error])
 if __name__ == '__main__':
     file_code_font = 'exemplos.md'
 
     scanner = Scanner(file_code_font)
     tokens, errors = scanner.analyse()
 
-    print("\nTokens Válidos \n")
-    for token in tokens:
-        print('<{}, {}>'.format(token[0], token[1]))
+    if errors:
+        for error in errors:
+            print(f"Erro léxico na linha {error[0]}: {error[1]}")
+        exit(1)
 
-    # print("\nTokens Inválidos: \n")
-    # for error in errors:
-    #     print(errors[error])
+    parser = Parser(tokens)
+    try:
+        parse_tree = parser.parse()
+        print("Análise sintática concluída com sucesso!")
+    except SyntaxError as e:
+        print(f"Erro sintático: {str(e)}")
